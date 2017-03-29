93cbcfa885219b9cf2668128c443e8c83ec6850b
sunming <sunming@haima.me>
2017-03-29 16:38:22 +0800

    Initial commit

---
+++ src/test/java/sun/agg/redis/TestAnnotationRedisImplTest.java
@@ -0,0 +1,27 @@
+package sun.agg.redis;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by sunming on 2016/11/10.
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = "classpath:/spring/applicationContext-*.xml")
+public class TestAnnotationRedisImplTest {
+
+//    @Autowired
+//    private DemoAnnotationRedisImpl testAnnotationRedisImpl;
+//
+//    @Test
+//    public void sum(){
+//        System.out.println("aaaaaaaaaaaaaaaaaaaaa"+testAnnotationRedisImpl.sum(109,3));
+//        System.out.println("aaaaaaaaaaaaaaaaaaaaa"+testAnnotationRedisImpl.sum(109,3));
+//        System.out.println("aaaaaaaaaaaaaaaaaaaaa"+testAnnotationRedisImpl.sum(100,2));
+//    System.out.println("aaaaaaaaaaaaaaaaaaaaa"+testAnnotationRedisImpl.sum(100,3));
+//}
+
+}
